d942f69b95e1e6d25d57870b01e65c6652c7b6d5
Vitor Domingos <vitorwalisson025@gmail.com>
2023-08-06 21:11:04 -0300

    Eventos de teclado e melhorias

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 //Toca o som através do parametro recebido
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
 
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento != null && elemento.localName === 'audio') {
+
+        elemento.play();
+    } else{
+
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 //Captura todas as tecla do HTML e guarda em uma lista
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     //Armazena cada tecla em uma constante
     const tecla = listaDeTeclas[contador];
@@ -20,10 +26,23 @@ while (contador < listaDeTeclas.length){
     //Cria um template string para cada instrumento
     const idAudio = `#som_${instrumento}`;
 
+    //Função anonima
     tecla.onclick = function () {
 
         tocaSom(idAudio);
-    };
+    }
 
-    contador++;
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+
+        tecla.classList.remove('ativa');
+    }
+
 }
